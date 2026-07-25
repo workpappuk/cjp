2cc29612ff9e034119186ea4916242ee3e9353fe
Your Name <you@example.com>
2026-07-25 09:28:03 +0530

    fc

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
-
-
-class Todo(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    title: str
-    completed: bool = False
